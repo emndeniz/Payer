b32c0e2dbcfcf52ec9953aff3eed012114c4b9d9
Emin DENİZ <emndeniz@gmail.com>
2022-09-20 23:55:09 +0300

    Initial Commit

---
+++ Payer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Payer
+//
+//  Created by Emin on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
